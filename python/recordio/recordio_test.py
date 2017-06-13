f762d844cf20931ad1262d42c1bed30b565d81ec
Helin Wang <ustc.harry@gmail.com>
2017-06-13 01:48:31 +0000

    fix issue that C buffer may be no longer valid when RecordIO dumps the buffer to disk

--- python/recordio/recordio_test.py
+++ python/recordio/recordio_test.py
@@ -1,12 +1,12 @@
 import unittest
 
 import recordio
-
+import cPickle as pickle
 
 class TestStringMethods(unittest.TestCase):
     def test_write_read(self):
         w = recordio.writer("/tmp/record_0")
-        w.write("1")
+        w.write(pickle.dumps("1"))
         w.write("2")
         w.write("")
         w.close()
@@ -17,7 +17,7 @@ class TestStringMethods(unittest.TestCase):
         w.close()
 
         r = recordio.reader("/tmp/record_*")
-        self.assertEqual(r.read(), "1")
+        self.assertEqual(pickle.loads(r.read()), "1")
         self.assertEqual(r.read(), "2")
         self.assertEqual(r.read(), "")
         self.assertEqual(r.read(), "3")
