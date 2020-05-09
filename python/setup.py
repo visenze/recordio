9da9f6f4ff3309b4c2f520bb46589a1922ae6000
Nguyen Q Hanh <hanhnq87@gmail.com>
2020-05-09 21:00:23 +0800

    version: 0.3

--- python/setup.py
+++ python/setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, Distribution
 
+
 class BinaryDistribution(Distribution):
     def has_ext_modules(foo):
         return True
 
+
 setup(
-    name='visenze_recordio',
-    version='0.2',
-    description='An implementation of the RecordIO file format.',
-    url='https://github.com/PaddlePaddle/recordio',
-    author='PaddlePaddle Authors',
-    author_email='paddle-dev@baidu.com',
-    license='Apache 2.0',
-    packages=['recordio'],
-    package_data={
-        'recordio': ['librecordio.so'],
-    },
-    distclass=BinaryDistribution
+    name="visenze_recordio",
+    version="0.3",
+    description="An implementation of the RecordIO file format.",
+    url="https://github.com/PaddlePaddle/recordio",
+    author="PaddlePaddle Authors",
+    author_email="paddle-dev@baidu.com",
+    license="Apache 2.0",
+    packages=["recordio"],
+    package_data={"recordio": ["librecordio.so"],},
+    distclass=BinaryDistribution,
 )
